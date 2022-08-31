9c262989a606eeb946c68fdb731978c00934d5ed
magnificientA <abdulalimzakariyah@gmail.com>
2022-08-31 15:27:26 +0100

    Login

---
+++ Music.js
@@ -0,0 +1,25 @@
+var musicians = 20;
+if (musicians === 1) {
+    console.log("solo");
+}// your code goes here
+else if (musicians === 2) {
+    console.log("duet");
+}// your code goes here
+else if (musicians === 3) {
+    console.log("trio");
+}// your code goes here
+else if (musicians === 4) {
+    console.log("quartet");
+}// your code goes here
+else if (musicians > 4) {
+    console.log("this is a large group");
+}// your code goes here
+
+else {
+    console.log("not a group");
+    
+}
+
+Output:
+this is a large group
+
